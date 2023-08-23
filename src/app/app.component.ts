4e3d5a899bf992d3ce26bd297612cf749af05b19
Martín Salas <martinsalas_9715@yahoo.com>
2023-08-22 21:51:49 -0500

    using property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  // title = 'first-app';
+  Storedposts = [];
+
+  onPostAdded(post: any) {
+    this.Storedposts.push(post);
+  }
 }
